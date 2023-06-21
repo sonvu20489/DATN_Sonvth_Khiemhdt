31f3fd2532c8b93e7aa537d8b93c334bdecef2e1
SonVTH <95125132+sonvu20489@users.noreply.github.com>
2023-06-21 23:33:55 +0700

    Delete connection.php

--- Code/Website/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server_username = "id20314831_admin";
-$server_password = "Khoacuaaninh123@@";
-$server_host = "localhost";
-$database = 'id20314831_dbcuaanninh';
-
-$conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
-mysqli_query($conn,"SET NAMES 'UTF8'");
